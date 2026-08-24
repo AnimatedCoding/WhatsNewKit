fdef4ebb58508919715b9f32ae47ef0055230b9e
Noah-Davidson <kittenapps@icloud.com>
2026-08-24 12:46:10 +0500

    v2.0.10

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "WhatsNewKit",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
         .macOS(.v12),
         .visionOS(.v1)
     ],
